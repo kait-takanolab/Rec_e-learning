d3c1059ae1e9174b8430e4d908ed2c1988c8537a
kohei1110 <s1421172@cco.kanagawa-it.ac.jp>
2017-09-10 17:06:03 +0900

    録音するたびに音源を保存できるように機能修正

--- record_aoki/Recorderjs-master/get.php
+++ record_aoki/Recorderjs-master/get.php
@@ -1,18 +1,24 @@
 <?php
 
  $file=$_FILES['fname']['tmp_name'];
+ $url=$_POST['url'];
 
 // $filename=$_POST['fname'];
 
 //$decodeData=base64_decode($file);
 
-echo $file;
-echo $_FILES['fname']['name'];
+//echo $file;
+//echo $url;
+
+$urlpices=explode("/", $url);
+
+echo $urlpices[3];
+/*echo $_FILES['fname']['name'];
 echo $_FILES['fname']['type'];
 echo $_FILES['fname']['error'];
-echo $_FILES['fname']['size'];
+echo $_FILES['fname']['size']; */
 //echo $file;
-rename($file,"./uploaded_audio2.wav");
+rename($file,"./sample/$urlpices[3].wav");
 /*$fp=fopen($filename,'wb');
 fwrite($fp,$decodeData);
 fclose($fp);*/
