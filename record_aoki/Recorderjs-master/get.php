9e5c50e4eea1334aa586cad10489e4624a2d6a69
kohei1110 <s1421172@cco.kanagawa-it.ac.jp>
2017-09-21 11:31:20 +0900

    録音した音声を保存する際に、パーミッションを変更する機能を追加

--- record_aoki/Recorderjs-master/get.php
+++ record_aoki/Recorderjs-master/get.php
@@ -3,6 +3,9 @@
  $file=$_FILES['fname']['tmp_name'];
  $url=$_POST['url'];
 
+$perm_num=777;
+
+$perm=sprintf("%04d",$perm_num);
 // $filename=$_POST['fname'];
 
 //$decodeData=base64_decode($file);
@@ -19,6 +22,8 @@ echo $_FILES['fname']['error'];
 echo $_FILES['fname']['size']; */
 //echo $file;
 rename($file,"./sample/$urlpices[3].wav");
+chmod("./sample/$urlpices[3].wav", octdec($perm));
+
 /*$fp=fopen($filename,'wb');
 fwrite($fp,$decodeData);
 fclose($fp);*/
